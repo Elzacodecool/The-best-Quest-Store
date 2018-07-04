e8a68327823d3676dabd83388e54976a65d3f21e
polon59 <przemyslaw.polczak@gmail.com>
2018-07-04 13:44:45 +0200

    Add 6 user story query in .SQL file

---
+++ sql/SQL test queries/6.sql
@@ -0,0 +1,16 @@
+INSERT INTO user
+	(login, first_name, last_name, email, type)
+VALUES (
+	'specified login',
+	'sample first name',
+	'sample last name',
+	'sample email',
+	'specified type');
+
+
+INSERT INTO codecooler
+	(user_login, class_id, level_id)
+VALUES (
+	'same specified login',
+	'sample class id',
+	'sample level id',);
