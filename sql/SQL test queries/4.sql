9e4f9d91e1c17c0ecfd9238904d576f270f21432
polon59 <przemyslaw.polczak@gmail.com>
2018-07-04 14:55:38 +0200

    'Change SQL query in '4.sql file'

--- sql/SQL test queries/4.sql
+++ sql/SQL test queries/4.sql
@@ -3,11 +3,11 @@ WHERE id = sample_id;
 
 
 
+
 SELECT first_name, last_name, class_id FROM codecooler
 JOIN user
 ON user.login = codecooler.user_login
+
 WHERE class_id IN
-(SELECT class_id FROM mentor 
-JOIN mentor_class
-ON mentor.id = mentor_class.mentor_id
-WHERE user_login = sample_login;)
+(SELECT class_id FROM mentor_class 
+WHERE mentor_id = Specified_mentor_ID)
