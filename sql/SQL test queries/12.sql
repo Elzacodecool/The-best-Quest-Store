92e0b5bba55cbeea30091fa2ac7abc8144ba868c
polon59 <przemyslaw.polczak@gmail.com>
2018-07-05 00:55:07 +0200

    Reconstruct query in '12.sql' file to make it work properly

--- sql/SQL test queries/12.sql
+++ sql/SQL test queries/12.sql
@@ -1,5 +1,15 @@
-INSERT INTO codecooler_quest
-	(codecooler_id, quest_id, mark_date)
-VALUES (specified_codecooler_id,
-	specified quest_id,
-	mark_date); 
+
+UPDATE codecooler
+SET wallet = wallet + 
+(SELECT value FROM codecooler_quest
+JOIN quest
+ON codecooler_quest.quest_id = quest.id
+WHERE codecooler_quest.id = selected_done_quest_id)
+WHERE id = 
+(SELECT codecooler_id FROM codecooler_quest
+WHERE id = selected_done_quest_id);
+UPDATE codecooler_quest
+SET mark_date = current_date
+WHERE id = selected_done_quest_id;
+
+
