75c747ebc8a2bab362ce841c97d3695cad75d160
polon59 <przemyslaw.polczak@gmail.com>
2018-07-05 00:45:56 +0200

    Add table join statement in '4.sql' file to return complete mentor data

--- sql/SQL test queries/4.sql
+++ sql/SQL test queries/4.sql
@@ -1,13 +1,13 @@
 SELECT * FROM mentor
-WHERE id = sample_id;
-
+JOIN user_
+ON user_.login = mentor.user_login
+WHERE id = selected mentor_id;
 
 
 
 SELECT first_name, last_name, class_id FROM codecooler
-JOIN user
-ON user.login = codecooler.user_login
-
+JOIN user_
+ON user_.login = codecooler.user_login
 WHERE class_id IN
 (SELECT class_id FROM mentor_class 
-WHERE mentor_id = Specified_mentor_ID)
+WHERE mentor_id = selected_mentor_id)
