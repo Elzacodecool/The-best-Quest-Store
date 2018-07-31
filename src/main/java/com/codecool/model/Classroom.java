ba1e79c5570f057e442f0405f8a77710f13e9045
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-07-31 23:59:14 +0200

    Implement ClassroomDAO

--- src/main/java/com/codecool/model/Classroom.java
+++ src/main/java/com/codecool/model/Classroom.java
@@ -1,15 +1,24 @@
 package com.codecool.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Classroom {
 
-    private int id;
+    private Integer id;
     private String name;
 
     public Classroom(String name) {
         this.name = name;
     }
 
-    public int getId() {
+    // data from DB
+    public Classroom(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
         return this.id;
     }
 
@@ -19,5 +28,27 @@ public class Classroom {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String toString() {
+        return String.format("[Classroom] id: %s, name: %s", this.id, this.name);
+    }
+
+    // for sql query to insert classroom to DB
+    public Map<Integer, Object> toHashMap() {
+
+        Map<Integer, Object> classroomMap = new HashMap<>();
+        classroomMap.put(1, this.name);
+
+        return classroomMap;
+    }
+
+    // for sql query to update classroom in DB
+    public Map<Integer, Object> toHashMapWithId() {
+
+        Map<Integer, Object> classroomMap = toHashMap();
+        classroomMap.put(2, this.id);
+
+        return classroomMap;
     }
 }
