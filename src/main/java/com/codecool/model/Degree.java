e91564cf736b625b4ee72f258ba0cb6094b7dcf3
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-08-01 00:25:21 +0200

    Implement DegreeDAO

--- src/main/java/com/codecool/model/Degree.java
+++ src/main/java/com/codecool/model/Degree.java
@@ -1,17 +1,27 @@
 package com.codecool.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Degree {
 
-    private int id;
+    private Integer id;
     private String name;
-    private int minEarnedCoolcoins;
+    private Integer minEarnedCoolcoins;
 
-    public Degree(String name, int minEarnedCoolcoins) {
+    public Degree(String name, Integer minEarnedCoolcoins) {
         this.name = name;
         this.minEarnedCoolcoins = minEarnedCoolcoins;
     }
 
-    public int getId() {
+    // data from DB
+    public Degree(Integer id, String name, Integer minEarnedCoolcoins) {
+        this.id = id;
+        this.name = name;
+        this.minEarnedCoolcoins = minEarnedCoolcoins;
+    }
+
+    public Integer getId() {
         return this.id;
     }
 
@@ -23,11 +33,35 @@ public class Degree {
         this.name = name;
     }
 
-    public int getMinEarnedCoolcoins() {
+    public Integer getMinEarnedCoolcoins() {
         return this.minEarnedCoolcoins;
     }
 
-    public void setMinEarnedCoolcoins(int minEarnedCoolcoins) {
+    public void setMinEarnedCoolcoins(Integer minEarnedCoolcoins) {
         this.minEarnedCoolcoins = minEarnedCoolcoins;
+    }
+
+    public String toString() {
+        return String.format("[Degree] id: %d, name: %s, minEarnedCoolcoins: %d", 
+                            this.id, this.name, this.minEarnedCoolcoins);
+    }
+
+    // for sql query to insert degree to DB
+    public Map<Integer, Object> toHashMap() {
+
+        Map<Integer, Object> degreeMap = new HashMap<>();
+        degreeMap.put(1, this.name);
+        degreeMap.put(2, this.minEarnedCoolcoins);
+
+        return degreeMap;
+    }
+
+    // for sql query to update degree in DB
+    public Map<Integer, Object> toHashMapWithId() {
+
+        Map<Integer, Object> degreeMap = toHashMap();
+        degreeMap.put(3, this.id);
+
+        return degreeMap;
     }
 }
