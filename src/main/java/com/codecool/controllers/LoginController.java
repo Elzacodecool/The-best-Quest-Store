2d641dc7cfc279705614ac1809fa57709bd969a1
Elzacodecool <elliegolec@gmail.com>
2018-07-31 15:25:56 +0200

    Paste method parse data from request and redirect

--- src/main/java/com/codecool/controllers/LoginController.java
+++ src/main/java/com/codecool/controllers/LoginController.java
@@ -9,5 +9,40 @@ public class LoginController implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
+
+    private Map<String, String> getDataFromRequest(HttpExchange httpExchange) {
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(isr);
+
+        try {
+            String formData = br.readLine();
+            return parseFormData(formData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            // We have to decode the value because it's urlencoded. see: https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
+            String value = URLDecoder.decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+
+        return map;
+    }
+    private void redirect(HttpExchange httpExchange, String location) {
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.add("Location", location);
+        try {
+            httpExchange.sendResponseHeaders(302, -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        httpExchange.close();
     }
 }
