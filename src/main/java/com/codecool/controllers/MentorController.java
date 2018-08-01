d9b5b019b0c0438b8fdbb85bc01232d7f47bfb47
Elzacodecool <elliegolec@gmail.com>
2018-08-01 22:20:39 +0200

    Add log in by cookie

--- src/main/java/com/codecool/controllers/MentorController.java
+++ src/main/java/com/codecool/controllers/MentorController.java
@@ -1,15 +1,58 @@
 package com.codecool.controllers;
 
+import com.codecool.model.FactoryDAO;
+import com.codecool.model.Mentor;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpCookie;
+import java.util.List;
 
 public class MentorController implements HttpHandler {
+    private Mentor mentor;
+    private FactoryDAO factoryDAO = new FactoryDAO();
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        login(httpExchange);
+        String response = createResponse("static/templates/mentor_profile.twig");
+
+
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+
+    }
+
+    private String createResponse(String filepath) {
+        JtwigModel jtwigModel = JtwigModel.newModel();
+        jtwigModel.with("mentor", mentor);
+        JtwigTemplate jtwigTemplate = JtwigTemplate.classpathTemplate(filepath);
+        return jtwigTemplate.render(jtwigModel);
+    }
+
+    private void login(HttpExchange httpExchange) {
+        if (mentor != null) {
+            return;
+        }
+        String login = getLogin(getCookie(httpExchange));
+        List<Mentor> mentors = factoryDAO.getMentorDAO().getList();
+        for (Mentor mentor : mentors) {
+            if (mentor.getLogin().equals(login)) {
+                this.mentor = mentor;
+                return;
+            }
+        }
+        redirect(httpExchange, "login");
+    }
+
+    private HttpCookie getCookie(HttpExchange httpExchange) {
         String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
         HttpCookie cookie;
 
@@ -19,7 +62,11 @@ public class MentorController implements HttpHandler {
             cookie = null;
             redirect(httpExchange, "login");
         }
-        System.out.println(cookie.toString());
+        return cookie;
+    }
+
+    private String getLogin(HttpCookie cookie) {
+        return cookie.toString().split("=")[1];
     }
 
     private void redirect(HttpExchange httpExchange, String location) {
