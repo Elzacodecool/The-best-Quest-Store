241ed098cc402fde42dd80a3c37b9aebb60cff5f
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-08-01 10:44:18 +0200

    Add toString and toHashMap methods

--- src/main/java/com/codecool/model/AppUser.java
+++ src/main/java/com/codecool/model/AppUser.java
@@ -1,5 +1,8 @@
 package com.codecool.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AppUser {
 
     protected String login;
@@ -56,5 +59,37 @@ public class AppUser {
 
     public String getAppuserType() {
         return this.appuserType;
+    }
+
+    public String toString() {
+        return String.format("[AppUser] login: %s, password: %s, firstName: %s, lastName: %s, email: %s, appuserType: %s", 
+                                this.login, this.password, this.firstName, this.lastName, this.email, this.appuserType);
+    }
+
+    // for sql query to insert appUser to DB
+    public Map<Integer, Object> toHashMap() {
+
+        Map<Integer, Object> appUserMap = new HashMap<>();
+        appUserMap.put(1, this.login);
+        appUserMap.put(2, this.password);
+        appUserMap.put(3, this.firstName);
+        appUserMap.put(4, this.lastName);
+        appUserMap.put(5, this.email);
+        appUserMap.put(6, this.appuserType);
+
+        return appUserMap;
+    }
+
+    // for sql query to insert appUser to DB
+    public Map<Integer, Object> toHashMapToUpdate() {
+
+        Map<Integer, Object> appUserMap = new HashMap<>();
+        appUserMap.put(1, this.password);
+        appUserMap.put(2, this.firstName);
+        appUserMap.put(3, this.lastName);
+        appUserMap.put(4, this.email);
+        appUserMap.put(5, this.login);
+
+        return appUserMap;
     }
 }
