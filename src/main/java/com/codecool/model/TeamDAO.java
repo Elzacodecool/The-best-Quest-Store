51979d0926dba388389686757ce230b1d15b5db2
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-08-01 10:57:53 +0200

    Add frames for rest DAOs

---
+++ src/main/java/com/codecool/model/TeamDAO.java
@@ -0,0 +1,28 @@
+package com.codecool.model;
+
+public class TeamDAO {
+    
+    public void add(Team team) {
+
+    }
+
+    public void update(Team team) {
+
+    }
+
+    public void delete(Team team) {
+
+    }
+
+    public void get(int id) {
+
+    }
+
+    public void getList() {
+        
+    }
+
+    public void addCodecoolerToTeam(Codecooler codecooler, Integer coolcoins) {
+
+    }
+}
